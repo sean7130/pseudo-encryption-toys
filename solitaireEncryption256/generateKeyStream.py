7e2024f5d54dab39943ff6fd3c84a5c2fd130d74
Yi Jing Tong <sean.tong@mail.utoronto.ca>
2020-12-14 10:39:30 -0500

    completed POC for generateKeystream.py, command line argument phrasing to be added

--- solitaireEncryption256/generateKeyStream.py
+++ solitaireEncryption256/generateKeyStream.py
@@ -5,7 +5,7 @@ def show_help(exit=True):
     print("usage: " + sys.argv[0] + " [-f filename] [-s size]")
     if exit: exit()
 
-def generate_keystream(size, seed=None):
+def generate_keystream(size=256, seed=None):
     ky = list()
     for i in range(1, size+1):
         ky.append(i)
@@ -21,16 +21,21 @@ def write_keystream_to_file(keystream, filename='ks'):
     fd = open(filename, 'w')
     stream = ""
     for e in keystream: 
-        stream += (e + " ")
+        stream += (str(e) + " ")
     fd.write(stream)
     fd.flush()
     fd.close()
 
 
+# Main function
+def generate_keystream_to_file(size=256, seed=None, filename='ks'):
+    ks = generate_keystream(size, seed)
+    write_keystream_to_file(ks, filename)
+
+
 if __name__ == "__main__":
     if "--help" in sys.argv:
         show_help()
-    # TODO: add command line flag support for -f and -s
 
-    generate_keystream(256)
-
+    # TODO: refine command line flag support for -f -s (seed) --seed --size
+    generate_keystream_to_file()
